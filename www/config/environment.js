46dcf7cd1f2cf93ada1164af90403b0dd5bc07a0
Coin Miner <bitminings@naver.com>
2018-02-11 13:49:15 +0900

    Fee increase

--- www/config/environment.js
+++ www/config/environment.js
@@ -29,7 +29,7 @@ module.exports = function(environment) {
       StratumPort: 8008,
 
       // Fee and payout details
-      PoolFee: '면제',
+      PoolFee: '1.0%',
       PayoutThreshold: '1 ESC',
 
       // For network hashrate (change for your favourite fork)
